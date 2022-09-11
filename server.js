cbf53dd880468494d6fd71f5cc3e7edfa399902b
RobinHerzig <robin.hrzg@gmail.com>
2022-09-11 18:13:08 -0500

    Update database name

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'CAD'
+    dbName = 'cad'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
